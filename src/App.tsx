1b77741010c7088abf2a904ac6a952992a5d5803
KURUMI <k.1au12um1@gmail.com>
2024-09-06 06:59:07 +0900

    add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     こんにちは！
+      <ul>
+        <li>TODOリスト
+          <button>追加</button>
+          <button>削除</button>
+        </li>
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
